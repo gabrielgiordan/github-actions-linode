6ce0ab8f3b50a27cd73eead9666c876564d30d34
Gabriel Medina <hello@gabrielgiordano.com>
2020-05-01 23:26:50 -0300

    Add Terraform linode_instance resource

---
+++ README.md
@@ -0,0 +1,5 @@
+# Requirements
+
+- Terrafrom 0.12 >
+
+In order to support the Terraform version 0.12 or greater with `hcl2` support, install the [Terraform Language Server](https://github.com/mauve/vscode-terraform/issues/157#issuecomment-605020900)
---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+variable "linode_api_token" {
+  type        = string
+  description = "The Linode Token API"
+}
+
+variable "linode_public_ssh_key" {
+  type        = string
+  description = "The Linode public SSH Key"
+}
+
+provider "linode" {
+  token = var.linode_api_token
+}
+
+resource "linode_instance" "app_instance" {
+  label           = var.linode_instance_label
+  image           = "linode/alpine3.11"
+  type            = "g6-nanode-1"
+  region          = "us-southeast"
+  swap_size       = 512
+  tags            = ["production"]
+  authorized_keys = [var.linode_public_ssh_key]
+}
